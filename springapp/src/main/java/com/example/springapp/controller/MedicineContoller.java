9cd17136221d9018db598ec4597e37c94abf599d
Iamneo <techadmin@iamneo.ai>
2023-10-02 18:42:34 +0000

    Backup Commit

--- springapp/src/main/java/com/example/springapp/controller/MedicineContoller.java
+++ springapp/src/main/java/com/example/springapp/controller/MedicineContoller.java
@@ -4,11 +4,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class MedicineContoller {
     private List<Medicine> medicineList = new ArrayList<>();
 
     // POST endpoint to add a new medicine
-    @PostMapping
+    @PostMapping("/")
     public boolean addMedicine(@RequestBody Medicine medicine) {
         return medicineList.add(medicine);
     }
